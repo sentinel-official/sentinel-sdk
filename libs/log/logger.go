2c0aeba55f047d316f160c8cd891521b46d771a1
Tony Stark <ironman0x7b2@protonmail.com>
2025-01-11 06:19:58 +0530

    feat: add a global log functions

--- libs/log/logger.go
+++ libs/log/logger.go
@@ -9,6 +9,45 @@ import (
 	"github.com/rs/zerolog"
 )
 
+var logger = log.NewNopLogger() // Default to a no-op logger
+
+// Info logs an informational message.
+func Info(msg string, keyVals ...any) {
+	logger.Info(msg, keyVals...)
+}
+
+// Warn logs a warning message.
+func Warn(msg string, keyVals ...any) {
+	logger.Warn(msg, keyVals...)
+}
+
+// Error logs an error message.
+func Error(msg string, keyVals ...any) {
+	logger.Error(msg, keyVals...)
+}
+
+// Debug logs a debug message.
+func Debug(msg string, keyVals ...any) {
+	logger.Debug(msg, keyVals...)
+}
+
+// With returns a logger with additional context.
+func With(keyVals ...any) log.Logger {
+	return logger.With(keyVals...)
+}
+
+// Impl returns the underlying implementation of the logger.
+func Impl() any {
+	return logger.Impl()
+}
+
+// SetLogger sets the global logger instance.
+func SetLogger(l log.Logger) {
+	if l != nil {
+		logger = l
+	}
+}
+
 // NewLogger creates a new logger instance with the specified output writer, format, and log level.
 func NewLogger(w io.Writer, format, level string) (log.Logger, error) {
 	// Parse the log level from the string
